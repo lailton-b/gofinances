8947ea946097e573fa7dcb75f1d61632341845b4
Lailton <lailton_b@hotmail.com>
2022-03-11 14:59:53 -0300

    feat: connecting with google and apple

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
-import 'react-native-gesture-handler'
+// import 'react-native-gesture-handler'
 import 'intl'
 import 'intl/locale-data/jsonp/pt-BR'
-
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import AppLoading from 'expo-app-loading'
@@ -11,9 +10,11 @@ import {
   Poppins_500Medium, 
   Poppins_700Bold 
 } from '@expo-google-fonts/poppins'
+
 import { theme } from './src/global/styles/theme';
 import { AppRoutes } from './src/routes/app.routes';
-
+import { SignIn } from './src/screens/SignIn';
+import { AuthProvider } from './src/hooks/auth';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -28,7 +29,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <AppRoutes />
+      <AuthProvider>
+        <SignIn />
+      </AuthProvider>
     </ThemeProvider>
   );
 }
